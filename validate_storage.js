2a2cc01da201f53361417c7afa10ec6c2398d1f4
Monkumon1 <james.otunolah@gmail.com>
2026-07-05 15:18:10 +0100

    storage and validate

--- validate_storage.js
+++ validate_storage.js
@@ -5,21 +5,29 @@ import fs from "fs";
 const hostname = "127.0.0.1";
 const port = 3000;
 
+//file name to store the urls
 const FILE_NAME = "urls.json";
 
-export function readUrls() {
+//function to read the urls from the file and return them as an array
+function readUrls() {
+    
+//read the urls from the file, as many times as the function is called, and return them as an array
     let urls = [];
 
+//check if the file exists, if it does, read the data and parse it to json
     if (fs.existsSync(FILE_NAME)) {
         const data = fs.readFileSync(FILE_NAME, "utf8");
 
+//parse the data to json and store it in urls
         if (data) {
             urls = JSON.parse(data);
         }
     }
     return urls;
 }
-export function storeUrls(urls) {
+//function to store the urls in the file
+ function storeUrls(urls) {
+//write the urls to the file in json format, with 2 spaces indentation, and not modify any properties while converting (null, 2)
     fs.writeFileSync(
         FILE_NAME,
         JSON.stringify(urls, null, 2)
@@ -27,25 +35,23 @@ export function storeUrls(urls) {
 }
 
 //validation
+//function to validate the original url
 function validateUrl(originalUrl) {
-
+    
+//validate the originalurl is not empty
     if (!originalUrl) {
         return "Original URL is required.";
     }
-
-    if (typeof originalUrl !== "string") {
+//validate if the originalurl is a string
+    else if (typeof originalUrl !== "string") {
         return "Original URL must be a string.";
     }
 
     try {
         const parsedUrl = new URL(originalUrl);
-
-        if (
-            parsedUrl.protocol !== "http:" &&
-            parsedUrl.protocol !== "https:"
-        ) {
-            return "Only HTTP and HTTPS URLs are allowed.";
-        }
+//validate protocool
+        if (parsedUrl.protocol !== "http:" && parsedUrl.protocol !== "https:")
+            return;
 
     } catch (err) {
         return "Invalid URL format.";
@@ -53,4 +59,4 @@ function validateUrl(originalUrl) {
 
     return null;
 }
-console.log(null)
+//console.log(null)
